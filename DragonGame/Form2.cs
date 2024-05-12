5953349bdf3744606ba82f549786a7d2f1bc5e0c
Looweez <louiselee2005@gmail.com>
2024-05-12 11:50:08 +0200

    trying to do q.2.5 and failing

--- DragonGame/Form2.cs
+++ DragonGame/Form2.cs
@@ -12,9 +12,46 @@ namespace DragonGame
 {
     public partial class frmBattle : Form
     {
-        public frmBattle()
+        string playerName1;
+        string playerName2;
+        string dragonName1;
+        string dragonName2;
+        string dragonType1;
+        string dragonType2;
+        int dragonHP1;
+        int dragonHP2;
+        int dragonATK1;
+        int dragonATK2;
+        int dragonSPATK1;
+        int dragonSPATK2;
+        int dragonBLOCK1;
+        int dragonBLOCK2;
+
+        public frmBattle(int[] P1values, int[] P2values, string[] P1data, string[] P2data)
         {
             InitializeComponent();
+
+            dragonName1 = P1data[1];
+            dragonType1 = P1data[2];
+            gbxPlayer1Dragon.Text = dragonName1 + " the " + dragonType1 + " Dragon's turn";
+
+            dragonHP1 = P1values[0];
+            lblPlayerHP.Text = "HP: " + dragonHP1; //hp shows up as 0,, idk why :(
+            
         }
+
+
+        public class randomRollGenerator //method for random number from 1 to 6
+        {
+            private Random random = new Random();
+            
+            public int randomRoll()
+            {
+                return random.Next(1, 7);
+            }
+        }
+
+        
+
     }
 }
